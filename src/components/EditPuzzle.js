9b8853e697194ac4f303d5a542058c6ff42a43be
sonofdust <sonofdust@gmail.com>
2023-02-11 05:08:44 -0700

    Initial Commit

--- src/components/EditPuzzle.js
+++ src/components/EditPuzzle.js
@@ -3,12 +3,11 @@ import PuzzleGrid from "./PuzzleGrid";
 import GridSizeSelect from "./GridSizeSelect";
 import WordInput from "./WordInput";
 import WordList from "./WordList";
-import TitleInput from "./TitleInput";
 import DataContext from "../context/DataContext";
 import {useContext} from "react";
 
 const EditPuzzle = () => {
-  const {title, removeAll, grid, wordList} = useContext(DataContext);
+  const {removeAll, wordList} = useContext(DataContext);
   return (
     <div className="container">
       <section className="edit-container">
